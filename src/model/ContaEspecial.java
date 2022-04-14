3a8f4e41d1892ff0aac3c56ea5906d71291780cb
Paulo Henrique <p.henrique461@gmail.com>
2022-04-14 19:23:02 -0300

    Alteração nas classes ContaCorrente é ContaEspecial

--- src/model/ContaEspecial.java
+++ src/model/ContaEspecial.java
@@ -2,5 +2,26 @@ package model;
 
 public class ContaEspecial extends Conta {
 
-	
+	public ContaEspecial() {
+
+	}
+
+	public ContaEspecial(String numeroConta, String tipoDeConta, Double saldoConta, Double limiteContaEspecial,
+			String cartaoDeCredito, Double saldoCartaoDeCredito, char statusConta) {
+		super(numeroConta, tipoDeConta, saldoConta, limiteContaEspecial, cartaoDeCredito, saldoCartaoDeCredito,
+				statusConta);
+	}	
+
+	@Override
+	public boolean saque(String numeroConta, double saque) {
+		double saldo = getSaldoConta();
+		double limiteEspecial = getLimiteContaEspecial();
+		
+		if (saque > 0.0 && saque <= (saldo + limiteEspecial)) {
+			//saldo -= saque;
+			//atualizarSaldo(saldo);			
+			return true;
+		}
+		return false;
+	}	
 }

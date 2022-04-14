303e0112f0dcfe39987659d6933aa5b72e31412e
Roberto Brito <roberto.brito.oliveira@hotmail.com>
2022-04-14 19:18:57 -0300

    Criação da classe cliente com seus atributos

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,8 +1,65 @@
 package model;
 
+import java.util.Date;
 
 public class Cliente {
 	
+	private String nome;
+	private String cpf;
+	private Date dataNascimento;
+	private String telefone;
+	private Double rendaMensal;
 	
+	public Cliente() {
+		
+	}
 	
+	public Cliente(String nome, String cpf, Date dataNascimento, String telefone, Double rendaMensal) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.telefone = telefone;
+		this.rendaMensal = rendaMensal;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Double getRendaMensal() {
+		return rendaMensal;
+	}
+
+	public void setRendaMensal(Double rendaMensal) {
+		this.rendaMensal = rendaMensal;
+	}
 }
